f41b02c41c426f6455edba8d0d61af664f355afc
Mills Provosty <45856797+MillsProvosty@users.noreply.github.com>
2019-03-27 09:19:48 -0600

    Iteration 2 complete

---
+++ lib/show.rb
@@ -0,0 +1,18 @@
+class Show
+  attr_reader :name,
+              :creator,
+              :characters
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |char|
+      char.salary
+    end
+  end
+
+
+end
